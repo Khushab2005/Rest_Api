ccc59cf12e3b73fc9ffa308b8026d18d15bd3648
Khushab2005 <khushabch.tagline@gmail.com>
2025-07-29 18:11:14 +0530

    ok

--- Rest_Api/asgi.py
+++ Rest_Api/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for RestApi project.
+ASGI config for Rest_Api project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RestApi.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rest_Api.settings')
 
 application = get_asgi_application()
--- Rest_Api/urls.py
+++ Rest_Api/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for RestApi project.
+URL configuration for Rest_Api project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
